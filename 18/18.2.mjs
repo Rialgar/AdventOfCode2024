89034e67c42ffd34f0acc47adf8313ab674d1bfc
Benjamin Siegmund <rialgar42@gmail.com>
2024-12-18 08:24:43 +0100

    day 18 optimization

--- 18/18.2.mjs
+++ 18/18.2.mjs
@@ -19,10 +19,12 @@ import { a_star, initMap, readLines } from "../utils.mjs";
         console.log(i, lines.length);
         nextCoords = lines[i].split(',').map(v => parseInt(v));
         map.set(nextCoords[0], nextCoords[1], '#');
-        path = a_star(map, {x:0, y:0}, {x: map.width-1, y: map.height-1}, {
-            filter: fields => fields[0].data !== '#',
-            print: false,
-        });
+        if(path.fields.some(f => f.x === nextCoords[0] && f.y === nextCoords[1])){
+            path = a_star(map, {x:0, y:0}, {x: map.width-1, y: map.height-1}, {
+                filter: fields => fields[0].data !== '#',
+                print: false,
+            });
+        }
         i++;
     }
     console.log(i < lines.length, nextCoords.join(','));
